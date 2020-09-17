6226cf6c028dde86762d1edfa975de374b364895
chanxinguidao <693196756@qq.com>
2020-09-17 12:58:48 +0800

    222

--- hutool/src/main/java/cn/goktech/HelloGit.java
+++ hutool/src/main/java/cn/goktech/HelloGit.java
@@ -3,5 +3,6 @@ package cn.goktech;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git ");
+        System.out.println("hello git ");
     }
 }
